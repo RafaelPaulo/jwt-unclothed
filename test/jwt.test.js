7a3353eecd79e7fb85d4c6f8c8fc98ddd4b69a54
RafaelPaulo <rafael-sq@hotmail.com>
2021-05-12 22:11:23 -0300

    this code was found on an old computer

--- test/jwt.test.js
+++ test/jwt.test.js
@@ -1,4 +1,4 @@
-const createJwt = require('../index');
+const { createJwt, openJwt } = require('../index')
 
 test('create a jwt token', () => {
     const tokenExpected = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VybmFtZSI6InJhZmFlbCIsImVtYWlsIjoicmFmYWVsQGVtYWlsLmNvbSIsImxvdmVzIjpbImNvZGUiLCJ0byBmaXNoIiwidHYgc2VyaWVzIiwic29mdHdhcmUgZW5naW5lZXJpbmciXX0.VCLVKYURRj6Rt83YXec-HViHR_TDevEKHZYFnI0kYWY'
@@ -12,4 +12,18 @@ test('create a jwt token', () => {
     const token = createJwt(payload, secret)
 
     expect(token).toBe(tokenExpected)
+})
+
+test('open a jwt token', () => {
+    const token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VybmFtZSI6InJhZmFlbCIsImVtYWlsIjoicmFmYWVsQGVtYWlsLmNvbSIsImxvdmVzIjpbImNvZGUiLCJ0byBmaXNoIiwidHYgc2VyaWVzIiwic29mdHdhcmUgZW5naW5lZXJpbmciXX0.VCLVKYURRj6Rt83YXec-HViHR_TDevEKHZYFnI0kYWY'
+    const payloadExpected = {
+        username: 'rafael',
+        email: 'rafael@email.com',
+        loves: [ 'code', 'to fish', 'tv series', 'software engineering' ]
+    }
+    const secret = 'rafael100'
+
+    const payload = openJwt(token, secret)
+
+    expect(payload).toBe(payloadExpected)
 })
